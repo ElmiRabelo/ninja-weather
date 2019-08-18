1c38ef4b186ba0b4897e42c9d6ae300578fa3944
Elmi Rabelo <rabeloelmi@gmail.com>
2019-08-18 01:56:00 -0300

    textos de html atualizdos e codigo em JS alterado para puxar dados em portugues da API

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,39 +1,40 @@
-class Forecast{
-  constructor(){
-    this.key = 'jt2AXMcIwzbG4YvvVs16EypGiSAS6FYz';
-    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  }
+class Forecast {
+	constructor() {
+		this.key = "jt2AXMcIwzbG4YvvVs16EypGiSAS6FYz";
+		this.weatherURI =
+			"http://dataservice.accuweather.com/currentconditions/v1/";
+		this.cityURI =
+			"http://dataservice.accuweather.com/locations/v1/cities/search";
+	}
 
-  async updateCity(city){
-    const cityDets = await this.getCity(city);
-    const weather = await this.getWeather(cityDets.Key);
+	async updateCity(city) {
+		const cityDets = await this.getCity(city);
+		const weather = await this.getWeather(cityDets.Key);
 
-    //object shorthand notation
-    return {cityDets, weather};
-  }
+		//object shorthand notation
+		return { cityDets, weather };
+	}
 
-  async getCity(city){
-    //query paramenters
-    const query = `?apikey=${this.key}&q=${city}`;
+	async getCity(city) {
+		//query paramenters
+		const query = `?apikey=${this.key}&q=${city}&language=pt-br`;
 
-    // wait until have a response
-    const response = await fetch(this.cityURI + query);
-    // json method returns a promise, in that case we gonna wait
-    const data = await response.json();
-    //getCity is a async function thus it returns a promise, so we can use then
-    return data[0];
-  }
+		// wait until have a response
+		const response = await fetch(this.cityURI + query);
+		// json method returns a promise, in that case we gonna wait
+		const data = await response.json();
+		//getCity is a async function thus it returns a promise, so we can use then
+		return data[0];
+	}
 
-  async getWeather(id){
-  const query = `${id}?apikey=${this.key}`;
+	async getWeather(id) {
+		const query = `${id}?apikey=${this.key}&language=pt-br`;
 
-  const response = await fetch(this.weatherURI + query);
-  const data = await response.json();
+		const response = await fetch(this.weatherURI + query);
+		const data = await response.json();
 
-  return data[0];
-  }
+		return data[0];
+	}
 
-//end class
+	//end class
 }
-
