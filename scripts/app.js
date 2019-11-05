6f340226c6172ef52f0438cc7628a27ee03b2ddd
Elmi Rabelo <rabeloelmi@gmail.com>
2019-11-05 11:49:58 -0300

    Bug fix em app.js que estava faltando uma parenteses

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,14 @@ const icon = document.querySelector(".icon img");
 const forecast = new Forecast();
 
 const updateUI = data => {
-	// const cityDets = data.cityDets;
-	// const weather = data.weather;
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
 
-	//destructure properties
-	const { cityDets, weather } = data;
+  //destructure properties
+  const { cityDets, weather } = data;
 
-	//Put information on the ui
-	details.innerHTML = `
+  //Put information on the ui
+  details.innerHTML = `
   <h5 class="my-3">${cityDets.EnglishName}</h5>
   <div class="my-3">${weather.WeatherText}</div>
   <div class="display-4 my-4">
@@ -22,46 +22,45 @@ const updateUI = data => {
   </div>
   `;
 
-	//show on the browser
-	if (card.classList.contains("d-none")) {
-		card.classList.remove("d-none");
-	}
+  //show on the browser
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
 
-	//update day/night images and icon time
+  //update day/night images and icon time
 
-	/////icon time
-	const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-	icon.setAttribute("src", iconSrc);
+  /////icon time
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
 
-	////////time imag
-	let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  ////////time imag
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
 
-
-	time.setAttribute("src", timeSrc);
-
+  time.setAttribute("src", timeSrc);
+};
 
 //city form event
 cityForm.addEventListener("submit", e => {
-	e.preventDefault();
+  e.preventDefault();
 
-	//get city value from input
-	const city = cityForm.city.value.trim();
-	cityForm.reset();
+  //get city value from input
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
 
-	//obtain all the necessary information with updateCity that takes the city input as argument
-	//update de ui with new city
-	forecast
-		.updateCity(city)
-		.then(data => updateUI(data))
-		.catch(err => console.log("Something went wrong.", err));
+  //obtain all the necessary information with updateCity that takes the city input as argument
+  //update de ui with new city
+  forecast
+    .updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log("Something went wrong.", err));
 
-	//set local storage
-	localStorage.setItem("city", city);
+  //set local storage
+  localStorage.setItem("city", city);
 });
 
 if (localStorage.getItem("city")) {
-	forecast
-		.updateCity(localStorage.getItem("city"))
-		.then(data => updateUI(data))
-		.catch(err => console.log(err));
+  forecast
+    .updateCity(localStorage.getItem("city"))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 }
